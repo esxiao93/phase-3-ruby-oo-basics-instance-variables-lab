1523e0bdddcb9b78f9ff83e3515fd6f3e1bc7c1b
Eric Xiao <es.xiao93@gmail.com>
2022-05-23 13:52:49 -0400

    commit tree 7904a542e41d81f192b537882d6bf7ec0691fc45

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
 
-end
+    def name=(value)
+      @name = value
+    end
+  
+    def name
+      @name
+    end
+  end
+  
+  oddie = Dog.new
+  oddie.name = "Oddie"
+  
+  puts oddie.name
